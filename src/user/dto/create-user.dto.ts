4b44956dd3308f7846b49da7fb9ea2744d094031
Temirbaev Temirlan <temirlan213@bk.ru>
2023-12-03 18:18:16 +0600

    feature/validation

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,9 +1,14 @@
 import { ApiProperty } from "@nestjs/swagger"
+import { IsString, Matches, MinLength } from "class-validator"
 
 export class CreateUserDto {
   @ApiProperty({ example: "0x557612fAbFe26F97bD7f6b6C0c11C21413A7366E" })
+  @IsString()
+  @Matches(/^0x[a-fA-F0-9]{40}$/g)
   readonly address: string
   @ApiProperty({ example: "test12345" })
+  @IsString()
+  @MinLength(5)
   readonly password: string
   @ApiProperty({ example: "foofie213" })
   readonly nickname: string
