73e7bcda0e6c7d77a8770d96f702a7b781d26952
Temirbaev Temirlan <temirlan213@bk.ru>
2023-12-22 00:08:31 +0600

    hotfix/cors

--- src/sockets/websocket.gateway.ts
+++ src/sockets/websocket.gateway.ts
@@ -7,28 +7,27 @@ import {
   OnGatewayDisconnect,
   WebSocketServer,
 } from "@nestjs/websockets"
-import {Server, Socket} from "socket.io"
-import {Message} from "../entities/message"
-import {InjectRepository} from "@nestjs/typeorm";
-import {Conversation} from "../entities/conversation";
-import {Repository} from "typeorm";
-import {HttpException, HttpStatus, UnauthorizedException} from "@nestjs/common";
-import {JwtService} from "@nestjs/jwt";
+import { Server, Socket } from "socket.io"
+import { Message } from "../entities/message"
+import { InjectRepository } from "@nestjs/typeorm"
+import { Conversation } from "../entities/conversation"
+import { Repository } from "typeorm"
+import { HttpException, HttpStatus, UnauthorizedException } from "@nestjs/common"
+import { JwtService } from "@nestjs/jwt"
 
-@WebSocketGateway()
+@WebSocketGateway({ cors: true })
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server
 
   private conversationClients = new Map<string, Set<Socket>>()
 
-  constructor(@InjectRepository(Conversation) private conversationRepository: Repository<Conversation>, private jwtService: JwtService) {
-  }
+  constructor(@InjectRepository(Conversation) private conversationRepository: Repository<Conversation>, private jwtService: JwtService) {}
 
   async handleConnection(client: Socket) {
     try {
       const conversationId = client.handshake.query.conversationId
-      const token = client.handshake.headers.token;
+      const token = client.handshake.headers.token
       if (!token) {
         throw new UnauthorizedException("You aren't authorized!")
       }
@@ -36,16 +35,15 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
         throw new HttpException("You didn't pass conversation ID", HttpStatus.BAD_REQUEST)
       }
       const conversation = await this.conversationRepository.findOne({
-        where: {id: Number(conversationId),},
-        relations: ["users"]
+        where: { id: Number(conversationId) },
+        relations: ["users"],
       })
 
       if (!conversation) {
-        throw new HttpException("Conversation not found", HttpStatus.NOT_FOUND);
+        throw new HttpException("Conversation not found", HttpStatus.NOT_FOUND)
       }
 
-      const decodedUser = this.jwtService.decode(String(token));
-
+      const decodedUser = this.jwtService.decode(String(token))
 
       const userIds = conversation.users.map(user => user.id)
 
@@ -59,8 +57,8 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
       }
       this.conversationClients.get(String(conversationId)).add(client)
     } catch (error) {
-      console.error("Error in handleConnection:", error.message);
-      client.disconnect();
+      console.error("Error in handleConnection:", error.message)
+      client.disconnect()
     }
   }
 
